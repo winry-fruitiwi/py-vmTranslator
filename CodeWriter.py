2a0e05920db15c3a613ef131be1127943cf6016b
Winry <winry.tian@gmail.com>
2022-05-02 12:43:08 -0700

    add list format to arithmetic, part of push command families

--- CodeWriter.py
+++ CodeWriter.py
@@ -6,144 +6,162 @@
 class CodeWriter:
     def __init__(self):
         # keep track of the comparisons, which have labels
-        self.comparison_number = 0
+        self.comp_num = 0
 
     # translates arithmetic code, with a command type of C_ARITHMETIC.
     def translate_arithmetic(self, command):
-        assembly = f"// {command}\n"
+        assembly = [f'// {command}']
         # if the command is negative:
         if command == "neg":
-            assembly += "@SP\n" \
-                        "A=M-1\n" \
-                        "M=-M\n"
+            assembly.extend([
+                '@SP',    # goto register 0
+                'A=M-1',  # select *[SP-1]
+                'M=-M'    # negate the top of the stack
+            ])
 
         elif command == "not":
-            assembly += "@SP\n" \
-                        "A=M-1\n" \
-                        "M=!M\n"
+            assembly.extend(['@SP',     # goto register 0
+                             'A=M-1',   # select *(SP-1)
+                             'M=!M'     # not the top of the stack
+                             ])
 
         elif command == "and":
-            assembly += "@SP\n" \
-                        "A=M-1\n" \
-                        "D=M\n" \
-                        "@SP\n" \
-                        "A=M-1\n" \
-                        "A=A-1\n" \
-                        "M=D&M\n" \
-                        "@SP\n" \
-                        "M=M-1\n"
+            assembly.extend([
+                            '@SP',      # goto SP
+                            'A=M-1',    # goto *(SP-1)
+                            'D=M',      # D = *(SP-1)
+                            '@SP',      # go back to SP
+                            'A=M-1',    # goto *(SP-2)
+                            'A=A-1',
+                            'M=D&M',    # D&M produces a bitwise and of 16 bits
+                            '@SP',      # increment SP
+                            'M=M-1'
+                            ])
 
         elif command == "or":
-            assembly += "@SP\n" \
-                        "A=M-1\n" \
-                        "D=M\n" \
-                        "@SP\n" \
-                        "A=M-1\n" \
-                        "A=A-1\n" \
-                        "M=D|M\n" \
-                        "@SP\n" \
-                        "M=M-1\n"
+            assembly.extend([
+                            '@SP',      # goto SP
+                            'A=M-1',    # goto *(SP-1)
+                            'D=M',      # D = *(SP-1)
+                            '@SP',      # go back to SP
+                            'A=M-1',    # goto *(SP-2)
+                            'A=A-1',
+                            'M=D|M',    # D|M produces a bitwise or of 16 bits
+                            '@SP',      # increment SP
+                            'M=M-1'
+                            ])
 
         elif command == "add":
-            assembly += "@SP\n" \
-                        "A=M-1\n" \
-                        "D=M\n" \
-                        "@SP\n" \
-                        "A=M-1\n" \
-                        "A=A-1\n" \
-                        "M=D+M\n" \
-                        "@SP\n" \
-                        "M=M-1\n"
+            assembly.extend([
+                        '@SP',          # goto SP
+                        'A=M-1',        # goto *(SP-1)
+                        'D=M',          # D = *(SP-1)
+                        '@SP',          # go back to SP
+                        'A=M-1',        # goto *(SP-2)
+                        'A=A-1',
+                        'M=D+M',        # D+M produces addition of 16 bits
+                        '@SP',          # increment SP
+                        'M=M-1'
+            ])
 
         elif command == "sub":
-            assembly += "@SP\n" \
-                        "A=M-1\n" \
-                        "D=M\n" \
-                        "@SP\n" \
-                        "A=M-1\n" \
-                        "A=A-1\n" \
-                        "M=M-D\n" \
-                        "@SP\n" \
-                        "M=M-1\n"
-
+            assembly.extend([
+                        '@SP',          # goto SP
+                        'A=M-1',        # goto *(SP-1)
+                        'D=M',          # D = *(SP-1)
+                        '@SP',          # go back to SP
+                        'A=M-1',        # goto *(SP-2)
+                        'A=A-1',
+                        'M=M-D',        # subtract D from M
+                        '@SP',          # increment SP
+                        'M=M-1'
+            ])
         elif command == "eq":
-            assembly += f"@SP\n" \
-                        f"AM=M-1\n" \
-                        f"D=M\n" \
-                        f"@SP\n" \
-                        f"A=M-1\n" \
-                        f"D=M-D\n" \
-                        f"@JUMP{self.comparison_number}.TRUE\n" \
-                        f"D;JEQ\n" \
-                        f"@JUMP{self.comparison_number}.FALSE\n" \
-                        f"0;JMP\n" \
-                        f"(JUMP{self.comparison_number}.TRUE)\n" \
-                        f"@SP\n" \
-                        f"A=M-1\n" \
-                        f"M=-1\n" \
-                        f"@EN{self.comparison_number}D\n" \
-                        f"0;JMP\n" \
-                        f"(JUMP{self.comparison_number}.FALSE)\n" \
-                        f"@SP\n" \
-                        f"A=M-1\n" \
-                        f"M=0\n" \
-                        f"(EN{self.comparison_number}D)\n"
-            self.comparison_number += 1
+            assembly.extend([
+                '@SP',                           # goto sp
+                'AM=M-1',                        # decrement sp, select *(SP)
+                'D=M',                           # D=*(SP)
+                '@SP',                           # goto SP
+                'A=M-1',                         # select *(SP-1)
+                'D=M-D',                         # D=*(SP-1)-*(SP)
+                f'@JUMP{self.comp_num}.TRUE',    # if D=0, jump to JUMP.TRUE
+                'D;JEQ',
+                f'@JUMP{self.comp_num}.FALSE',   # else, jump to JUMP.FALSE
+                '0;JMP',
+                f'(JUMP{self.comp_num}.TRUE)',   # set SP-1 to true (-1)
+                '@SP',
+                'A=M-1',
+                'M=-1',
+                f'@EN{self.comp_num}D',          # don't set SP-1 to false (0)
+                '0;JMP',
+                f'(JUMP{self.comp_num}.FALSE)',  # set SP-1 to false (0)
+                '@SP',
+                'A=M-1',
+                'M=0',
+                f'(EN{self.comp_num}D)'          # end of command
+            ]
+            )
+            self.comp_num += 1
 
         elif command == "lt":
-            assembly += f"@SP\n" \
-                        f"AM=M-1\n" \
-                        f"D=M\n" \
-                        f"@SP\n" \
-                        f"A=M-1\n" \
-                        f"D=M-D\n" \
-                        f"@JUMP{self.comparison_number}.TRUE\n" \
-                        f"D;JLT\n" \
-                        f"@JUMP{self.comparison_number}.FALSE\n" \
-                        f"0;JMP\n" \
-                        f"(JUMP{self.comparison_number}.TRUE)\n" \
-                        f"@SP\n" \
-                        f"A=M-1\n" \
-                        f"M=-1\n" \
-                        f"@EN{self.comparison_number}D\n" \
-                        f"0;JMP\n" \
-                        f"(JUMP{self.comparison_number}.FALSE)\n" \
-                        f"@SP\n" \
-                        f"A=M-1\n" \
-                        f"M=0\n" \
-                        f"(EN{self.comparison_number}D)\n"
-            self.comparison_number += 1
+            assembly.extend([
+                "@SP"                            # goto sp
+                "AM=M-1"                         # decrement sp, select *(SP)
+                "D=M"                            # D=*(SP)
+                "@SP"                            # goto SP
+                "A=M-1"                          # select *(SP-1)
+                "D=M-D"                          # D=*(SP-1)-*(SP)
+                f"@JUMP{self.comp_num}.TRUE"     # if D<0, jump to JUMP.TRUE
+                "D;JLT"                          
+                f"@JUMP{self.comp_num}.FALSE"    # else, jump to JUMP.FALSE
+                "0;JMP"                          
+                f"(JUMP{self.comp_num}.TRUE)"    # set SP-1 to true (-1)
+                "@SP"                            
+                "A=M-1"                         
+                "M=-1"                           
+                f"@EN{self.comp_num}D"           # don't set SP-1 to false (0)
+                "0;JMP"                          
+                f"(JUMP{self.comp_num}.FALSE)"   # set SP-1 to false (0)
+                "@SP"                            
+                "A=M-1"                          
+                "M=0"                            
+                f"(EN{self.comp_num}D)\n"        # end of command
+                ])
+            self.comp_num += 1
 
         elif command == "gt":
-            assembly += f"@SP\n" \
-                        f"AM=M-1\n" \
-                        f"D=M\n" \
-                        f"@SP\n" \
-                        f"A=M-1\n" \
-                        f"D=M-D\n" \
-                        f"@JUMP{self.comparison_number}.TRUE\n" \
-                        f"D;JGT\n" \
-                        f"@JUMP{self.comparison_number}.FALSE\n" \
-                        f"0;JMP\n" \
-                        f"(JUMP{self.comparison_number}.TRUE)\n" \
-                        f"@SP\n" \
-                        f"A=M-1\n" \
-                        f"M=-1\n" \
-                        f"@EN{self.comparison_number}D\n" \
-                        f"0;JMP\n" \
-                        f"(JUMP{self.comparison_number}.FALSE)\n" \
-                        f"@SP\n" \
-                        f"A=M-1\n" \
-                        f"M=0\n" \
-                        f"(EN{self.comparison_number}D)\n"
-            self.comparison_number += 1
+            assembly.extend([
+                "@SP"                            # goto sp
+                "AM=M-1"                         # decrement sp, select *(SP)
+                "D=M"                            # D=*(SP)
+                "@SP"                            # goto SP
+                "A=M-1"                          # select *(SP-1)
+                "D=M-D"                          # D=*(SP-1)-*(SP)
+                f"@JUMP{self.comp_num}.TRUE"     # if D>0, jump to JUMP.TRUE
+                "D;JGT"
+                f"@JUMP{self.comp_num}.FALSE"    # else, jump to JUMP.FALSE
+                "0;JMP"
+                f"(JUMP{self.comp_num}.TRUE)"    # set SP-1 to true (-1)
+                "@SP"
+                "A=M-1"
+                "M=-1"
+                f"@EN{self.comp_num}D"           # don't set SP-1 to false (0)
+                "0;JMP"
+                f"(JUMP{self.comp_num}.FALSE)"   # set SP-1 to false (0)
+                "@SP"
+                "A=M-1"
+                "M=0"
+                f"(EN{self.comp_num}D)\n"        # end of command
+            ])
+            self.comp_num += 1
 
-        print(assembly)
+        for assembly_command in assembly:
+            print(assembly_command)
 
     # translates memory access code, with command types of C_PUSH and C_POP.
     def translate_mem_access(self, command):
         command_breakdown = command.split(" ")
-        assembly = f"\n// {command}\n"
+        assembly = [f'// {command}']
 
         # print(command_breakdown)
 
@@ -152,64 +170,78 @@ class CodeWriter:
 
         if command_breakdown[0] == "push":
             if command_breakdown[1] == "argument":
-                assembly += f"@{i}\n" \
-                            "D=A\n" \
-                            "@ARG\n" \
-                            "A=M\n" \
-                            "D=D+A\n" \
-                            "@SP\n" \
-                            "A=M\n" \
-                            "M=D\n" \
-                            "@SP\n" \
-                            "M=M+1\n"
+                assembly.extend([
+                                f'@{i}',    # put i in the D register
+                                'D=A',
+                                '@ARG',     # goto *(ARG)
+                                'A=M',
+                                'D=D+A',    # return... this should be A=D+A
+                                # 'D=M',
+                                '@SP',      # goto *SP
+                                'A=M',
+                                'M=D',      # M = whatever D is
+                                '@SP',      # SP++
+                                'M=M+1'
+                                ])
 
             elif command_breakdown[1] == "this":
-                assembly += f"@{i}\n" \
-                            "D=A\n" \
-                            "@THIS\n" \
-                            "A=M\n" \
-                            "D=D+A\n" \
-                            "@SP\n" \
-                            "A=M\n" \
-                            "M=D\n" \
-                            "@SP\n" \
-                            "M=M+1\n"
+                assembly.extend([
+                                f"@{i}",    # put i in the D register
+                                "D=A",
+                                "@THIS",    # goto *(THIS)
+                                "A=M",
+                                "D=D+A",    # return... this should be A=D+A
+                                # 'D=M',
+                                "@SP",      # goto *SP
+                                "A=M",
+                                "M=D",      # M = whatever D is
+                                "@SP",      # SP++
+                                "M=M+1"
+                                ])
 
             elif command_breakdown[1] == "that":
-                assembly += f"@{i}\n" \
-                            "D=A\n" \
-                            "@THAT\n" \
-                            "A=M\n" \
-                            "D=D+A\n" \
-                            "@SP\n" \
-                            "A=M\n" \
-                            "M=D\n" \
-                            "@SP\n" \
-                            "M=M+1\n"
+                assembly.extend([
+                                f"@{i}",    # put i in the D register
+                                "D=A",
+                                "@THAT",    # goto *(THAT)
+                                "A=M",
+                                "D=D+A",    # return... this should be A=D+A
+                                # 'D=M',
+                                "@SP",      # goto *SP
+                                "A=M",
+                                "M=D",      # M = whatever D is
+                                "@SP",      # SP++
+                                "M=M+1"
+                                ])
 
             elif command_breakdown[1] == "local":
-                assembly += f"@{i}\n" \
-                            "D=A\n" \
-                            "@LCL\n" \
-                            "A=M\n" \
-                            "D=D+A\n" \
-                            "@SP\n" \
-                            "A=M\n" \
-                            "M=D\n" \
-                            "@SP\n" \
-                            "M=M+1\n"
+                assembly.extend([
+                                f"@{i}",    # put i in the D register
+                                "D=A",
+                                "@LCL",     # goto *(THAT)
+                                "A=M",
+                                "D=D+A",    # return... this should be A=D+A
+                                # 'D=M',
+                                "@SP",      # goto *SP
+                                "A=M",
+                                "M=D",      # M = whatever D is
+                                "@SP",      # SP++
+                                "M=M+1"
+                                ])
 
             elif command_breakdown[1] == "temp":
-                assembly += f"@{i}\n" \
-                            "D=A\n" \
-                            "@5\n" \
-                            "A=D+A\n" \
-                            "D=M\n" \
-                            "@SP\n" \
-                            "A=M\n" \
-                            "M=D\n" \
-                            "@SP\n" \
-                            "M=M+1\n"
+                assembly.extend([
+                                f"@{i}"     # put i in the D register
+                                "D=A"       
+                                "@5"        # 
+                                "A=D+A"     # 
+                                "D=M"       # 
+                                "@SP"       # 
+                                "A=M"       # 
+                                "M=D"       # 
+                                "@SP"       # 
+                                "M=M+1"     #
+                                ])
 
             elif command_breakdown[1] == "static":
                 assembly += f"@file.{i}\n" \
@@ -221,12 +253,12 @@ class CodeWriter:
                             "M=M+1"
 
             elif command_breakdown[1] == "constant":
-                assembly += f"@{i}\n"   \
-                            "D=A\n"   \
-                            "@SP\n"   \
-                            "A=M\n"   \
-                            "M=D\n"   \
-                            "@SP\n"   \
+                assembly += f"@{i}\n" \
+                            "D=A\n" \
+                            "@SP\n" \
+                            "A=M\n" \
+                            "M=D\n" \
+                            "@SP\n" \
                             "M=M+1\n"
 
             elif command_breakdown[1] == "pointer":
